e0158bffc7eba63aa4910929caeba56f01be6708
BeardOfNorris <michael.peterson3112@gmail.com>
2025-06-21 23:49:29 -0700

    finished ui changes

--- script.js
+++ script.js
@@ -6,51 +6,21 @@ function changeBackgroundColor(color) {
   document.body.style.backgroundColor = color;
 }
 
-// ABILITY TO ENABLE/DISABLE BUTTON JUST IN CASE I WANT TO ADD DELAY -
-// IN BUTTON CLICKABILITY IF I DECIDED TO ADD PLAYER/AI CHOICE IMAGE FOR A FEW SECONDS
-
-// const buttons = document.querySelectorAll("button");
-
-// const disableButton = function () {
-//   buttons.forEach((button) => {
-//     button.disabled = true;
-//   });
-// };
-
-// const enableButton = function () {
-//   buttons.forEach((button) => {
-//     button.disabled = false;
-//   });
-// };
-
 const resetOutcomeText = function () {
   document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
 };
 
 const resetScore = function () {
-  playerScore = "0";
-  computerScore = "0";
+  playerScore = 0;
+  computerScore = 0;
   document.getElementById("playerScore").innerHTML =
     "Player Score: " + playerScore;
   document.getElementById("computerScore").innerHTML =
     "Computer Score: " + computerScore;
 };
 
-const chooseRock = function () {
-  userInput = "rock";
-  console.log("rock");
-  playRound();
-};
-
-const choosePaper = function () {
-  userInput = "paper";
-  console.log("paper");
-  playRound();
-};
-
-const chooseScissors = function () {
-  userInput = "scissors";
-  console.log("scissors");
+const getPlayerChoice = function (choice) {
+  userInput = choice;
   playRound();
 };
 
@@ -77,63 +47,66 @@ const playRound = function () {
     document.getElementById("computerScore").innerHTML =
       "Computer Score: " + computerScore;
     changeBackgroundColor("#FFB6C1");
+    document.getElementById("choicesBar").innerHTML =
+      "Player chooses rock, computer chooses paper. Computer wins!";
     document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
   } else if (userInput === "rock" && computerChoice === "scissors") {
     playerScore += 1;
     document.getElementById("playerScore").innerHTML =
       "Player Score: " + playerScore;
     changeBackgroundColor("#90EE90");
+    document.getElementById("choicesBar").innerHTML =
+      "Player chooses rock, computer chooses scissors. Player wins!";
     document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
   } else if (userInput === "paper" && computerChoice === "rock") {
     playerScore += 1;
     document.getElementById("playerScore").innerHTML =
       "Player Score: " + playerScore;
     changeBackgroundColor("#90EE90");
+    document.getElementById("choicesBar").innerHTML =
+      "Player chooses paper, computer chooses rock. Player wins!";
     document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
   } else if (userInput === "paper" && computerChoice === "scissors") {
     computerScore += 1;
     document.getElementById("computerScore").innerHTML =
       "Computer Score: " + computerScore;
     changeBackgroundColor("#FFB6C1");
+    document.getElementById("choicesBar").innerHTML =
+      "Player chooses paper, computer chooses scissors. Computer wins!";
     document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
   } else if (userInput === "scissors" && computerChoice === "rock") {
     computerScore += 1;
     document.getElementById("computerScore").innerHTML =
       "Computer Score: " + computerScore;
     changeBackgroundColor("#FFB6C1");
+    document.getElementById("choicesBar").innerHTML =
+      "Player chooses scissors, computer chooses rock. Computer wins!";
     document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
   } else if (userInput === "scissors" && computerChoice === "paper") {
     playerScore += 1;
     document.getElementById("playerScore").innerHTML =
       "Player Score: " + playerScore;
     changeBackgroundColor("#90EE90");
+    document.getElementById("choicesBar").innerHTML =
+      "Player chooses scissors, computer chooses paper. Player wins!";
     document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
   } else if (userInput === computerChoice) {
     document.getElementById("outcomeText").innerHTML = "Rock, Paper, Scissors?";
     changeBackgroundColor("#FFFFE0");
+    document.getElementById("choicesBar").innerHTML = "TIE";
   }
   if (playerScore === 5) {
-    console.log("You win!");
-    playerScore = 0;
-    computerScore = 0;
-    document.getElementById("playerScore").innerHTML =
-      "Player Score: " + playerScore;
-    document.getElementById("computerScore").innerHTML =
-      "Computer Score: " + computerScore;
-    document.getElementById("outcomeText").innerHTML = "You win!";
+    document.getElementById("outcomeText").innerHTML = "Game over, You win!";
     setTimeout(resetOutcomeText, 5000);
     setTimeout(changeBackgroundColor("white"), 7000);
+    resetScore();
+    document.getElementById("choicesBar").innerHTML = "";
   } else if (computerScore === 5) {
-    console.log("You lose!");
-    playerScore = 0;
-    computerScore = 0;
-    document.getElementById("computerScore").innerHTML =
-      "Computer Score: " + computerScore;
-    document.getElementById("playerScore").innerHTML =
-      "Player Score: " + playerScore;
     document.getElementById("outcomeText").innerHTML = "Game over, you lose!";
     setTimeout(resetOutcomeText, 5000);
     setTimeout(changeBackgroundColor("white"), 7000);
+    resetScore();
+    document.getElementById("choicesBar").innerHTML = "";
   }
 };
 
@@ -142,3 +115,20 @@ document.getElementById("playerScore").innerHTML =
 
 document.getElementById("computerScore").innerHTML =
   "Computer Score: " + computerScore;
+
+// ABILITY TO ENABLE/DISABLE BUTTON JUST IN CASE I WANT TO ADD DELAY -
+// IN BUTTON CLICKABILITY IF I DECIDED TO ADD PLAYER/AI CHOICE IMAGE FOR A FEW SECONDS
+
+// const buttons = document.querySelectorAll("button");
+
+// const disableButton = function () {
+//   buttons.forEach((button) => {
+//     button.disabled = true;
+//   });
+// };
+
+// const enableButton = function () {
+//   buttons.forEach((button) => {
+//     button.disabled = false;
+//   });
+// };
